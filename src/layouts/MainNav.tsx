38a0ae552c1866da2a149490076b42b09f2238e0
Jeon Eonseok <jseokeon@gmail.com>
2023-09-24 17:05:49 +0900

    feat(mainNav): 메인 nav UI 구현

--- src/layouts/MainNav.tsx
+++ src/layouts/MainNav.tsx
@@ -1,48 +1,17 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
-import styled from 'styled-components';
-
-const MainNavigation = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  height: 14.6rem;
-  padding: 0 9.8rem;
-`;
-
-const MainLogoBox = styled(Link)`
-  display: flex;
-  gap: .9rem;
-  align-items: center;
-  justify-content: center;
-  font-size: 2rem;
-  font-weight: 700;
-  cursor: pointer;
-`;
-
-const CategoryList = styled.ul`
-  display: flex;
-  gap: 4.8rem;
-  align-items: center;
-  justify-content: center;
-  font-size: 1.6rem;
-
-  li, button { 
-    cursor: pointer;
-  }
-`;
+import * as S from './style';
 
 /** Main Navigation */
 const MainNav = () => {
   return (
-    <MainNavigation>
-      <MainLogoBox to="/">
+    <S.MainNavigation>
+      <S.MainLogoBox to="/">
         <StaticImage src="../assets/icons/mainLogo.svg" alt="main-logo" />
         <p>Eon-Seok&apos;s Devlog</p>
-      </MainLogoBox>
-      <CategoryList>
+      </S.MainLogoBox>
+      <S.CategoryList>
         <li>
           <Link to="#">All</Link>
         </li>
@@ -66,8 +35,8 @@ const MainNav = () => {
             <StaticImage src="../assets/icons/searchIc.svg" alt="search-icon" />
           </button>
         </li>
-      </CategoryList>
-    </MainNavigation>
+      </S.CategoryList>
+    </S.MainNavigation>
   );
 };
 
---
+++ src/layouts/style.ts
@@ -0,0 +1,39 @@
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const MainNavigation = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 14.6rem;
+  padding: 0 9.8rem;
+`;
+
+const MainLogoBox = styled(Link)`
+  display: flex;
+  gap: .9rem;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  font-weight: 700;
+  cursor: pointer;
+`;
+
+const CategoryList = styled.ul`
+  display: flex;
+  gap: 4.8rem;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.6rem;
+
+  li, button { 
+    cursor: pointer;
+  }
+`;
+
+export {
+  MainNavigation,
+  MainLogoBox,
+  CategoryList
+};
