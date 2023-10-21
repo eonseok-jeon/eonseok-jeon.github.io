94ff09500360ea368512190c0d3ce57da57555d2
Jeon Eonseok <jseokeon@gmail.com>
2023-10-22 00:59:46 +0900

    chore(heroSection): 메인 본문 수정

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as R from '@templates/ReusableCSS';
 import * as S from './style';
 
 /** Main Page Hero Section */
@@ -7,15 +6,14 @@ const HeroSection = ({ isSticky }: { isSticky: boolean }) => {
   return (
     <S.HeroContainer isSticky={isSticky}>
       <S.IntroductionBox>
-        <R.SubTitle>안녕!</R.SubTitle>
-        <S.MainTitle>나, 전언석은</S.MainTitle>
+        <S.MainTitle>나는 전언석이다.</S.MainTitle>
         <S.DescriptionBox>
           <p>1998년 대한민국, 서울에서 태어나</p>
           <p>현재 고양시에 거주 중인</p>
           <p>FE Developer를 꿈꾸는 26세 청년이다.</p>
           <br />
           <p>이곳은 나의 배움을 공유하는 공간이다.</p>
-          <p>무엇을 공유하는지 궁금한 사람은 아래의 버튼을 눌러보길 바란다.</p>
+          <p>무엇을 공유하는지 궁금한 사람은 아래의 구경하기 버튼을 눌러보길 바란다.</p>
           <br />
           <p>참고로 이곳의 모든 디자인과 코드는 <strong>내가 짰다.</strong></p>
         </S.DescriptionBox>
