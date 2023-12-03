93a83ba3703d5bdba4ef2d481c5bdd81f677d4c7
Jeon Eonseok <jseokeon@gmail.com>
2023-12-03 11:00:10 +0900

    design: post article 반응형 구현

--- src/templates/Post/style.ts
+++ src/templates/Post/style.ts
@@ -3,19 +3,20 @@ import * as R from '../ReusableCSS';
 
 const PostArticle = styled.article`
   max-width: 120rem;
-  margin: 8rem auto 0 ;
-  padding: 13.2rem 10rem 7rem; 
+  margin: 8rem auto 0;
+  padding: 7%;
 
   pre {
     display: inline-block;
+    width: 100%;
     overflow: auto;
     margin: 2rem 0;
-    padding: .1rem 2rem;
+    padding: 0.1rem 2rem;
     border-radius: 1rem;
     font-size: 1.2rem;
     color: #fff;
     background-color: #222;
-    border: .1rem solid #666;
+    border: 0.1rem solid #666;
 
     &::-webkit-scrollbar {
       display: none; /* Chrome, Safari, Opera*/
@@ -30,11 +31,7 @@ const PostMainTitleBox = styled.div`
 const PostSubTitle = styled(R.SubTitle)`
   padding: 2rem 0;
   font-weight: 500;
-  border-bottom: .1rem solid #ddd;
+  border-bottom: 0.1rem solid #ddd;
 `;
 
-export {
-  PostArticle,
-  PostMainTitleBox,
-  PostSubTitle
-};
+export { PostArticle, PostMainTitleBox, PostSubTitle };
